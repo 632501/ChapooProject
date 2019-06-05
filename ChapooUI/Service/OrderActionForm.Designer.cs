8ca29a0b1b878aba350ae0616db8c61e95bd70a6
Kevin Steinmetz <632501@student.inholland.nl>
2019-06-05 10:57:40 +0200

    Orderforms update

---
+++ ChapooUI/Service/OrderActionForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ChapooUI
+{
+    partial class OrderActionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnActionOpnemen = new System.Windows.Forms.Button();
+            this.btnActionBekijken = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnActionOpnemen
+            // 
+            this.btnActionOpnemen.Location = new System.Drawing.Point(52, 192);
+            this.btnActionOpnemen.Name = "btnActionOpnemen";
+            this.btnActionOpnemen.Size = new System.Drawing.Size(267, 118);
+            this.btnActionOpnemen.TabIndex = 0;
+            this.btnActionOpnemen.Text = "Bestelling opnemen";
+            this.btnActionOpnemen.UseVisualStyleBackColor = true;
+            this.btnActionOpnemen.Click += new System.EventHandler(this.btnActionOpnemen_Click);
+            // 
+            // btnActionBekijken
+            // 
+            this.btnActionBekijken.Location = new System.Drawing.Point(52, 357);
+            this.btnActionBekijken.Name = "btnActionBekijken";
+            this.btnActionBekijken.Size = new System.Drawing.Size(266, 118);
+            this.btnActionBekijken.TabIndex = 1;
+            this.btnActionBekijken.Text = "Bekijken en\r\nAanpassen\r\n";
+            this.btnActionBekijken.UseVisualStyleBackColor = true;
+            // 
+            // OrderActionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 566);
+            this.Controls.Add(this.btnActionBekijken);
+            this.Controls.Add(this.btnActionOpnemen);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "OrderActionForm";
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OrderForm";
+            this.Load += new System.EventHandler(this.OrderForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnActionOpnemen;
+        private System.Windows.Forms.Button btnActionBekijken;
+    }
+}
--- ChapooUI/Service/OrderMenusForm.cs
+++ ChapooUI/Service/OrderMenusForm.cs
@@ -9,17 +9,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ChapooLogica;
-using ChapooModel;
-using Menu = ChapooModel.Menu;
 
 namespace ChapooUI
 {
-    public partial class OrderForm : MaterialForm
+    public partial class OrderMenusForm : MaterialForm
     {
         private readonly MaterialSkinManager materialSkinManager;
 
-        public OrderForm()
+        public OrderMenusForm()
         {
             InitializeComponent();
 
@@ -32,31 +29,38 @@ namespace ChapooUI
 
         private void OrderForm_Load(object sender, EventArgs e)
         {
-            GetOrder();
+
         }
 
         private void listviewMenu_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
-    
-        public void GetOrder()
+
+
+        private void OrderMenusForm_Load(object sender, EventArgs e)
         {
-            mlblSoortBestelling.Text = "Drinken";
-            Menu_Service menuService = new Menu_Service();
-            List<Menu> menuList = menuService.GetDrinks();
 
-            listviewMenu.Items.Clear();
+        }
 
-            foreach (Menu m in menuList)
-            {
-                ListViewItem li = new ListViewItem(m.naam);
-                li.Tag = m;
-                listviewMenu.Items.Add(li);
+        private void btnDrinks_Click(object sender, EventArgs e)
+        {
+            OrderForm drinksForm = new OrderForm();
+        }
 
-            }
-            listviewMenu.View = View.Details;
-            listviewMenu.Columns.Add("Naam");
+        private void btnLunch_Click(object sender, EventArgs e)
+        {
+            OrderForm lunchForm = new OrderForm();
+        }
+
+        private void btnDiner_Click(object sender, EventArgs e)
+        {
+            OrderForm dinerForm = new OrderForm();
+        }
+
+        private void btnToevoegen_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
